f22654042d982439ca84c5852e831a87f2fabed8
Lorcan <ltravers92@gmail.com>
2026-04-20 20:28:12 -0700

    fix: jellyseerr caddy vhost to seer subdomain

--- modules/optiplex/overseerr.nix
+++ modules/optiplex/overseerr.nix
@@ -1,7 +1,7 @@
 { domain, ... }: {
   services.jellyseerr.enable = true; # port 5055
 
-  services.caddy.virtualHosts."overseerr.${domain}".extraConfig = ''
+  services.caddy.virtualHosts."seer.${domain}".extraConfig = ''
     import cloudflare_tls
     reverse_proxy localhost:5055
   '';
